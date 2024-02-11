94a11ee05e71abdb7f2cf595fc9ead51e6dc8797
adisingh925 <adisingh925@gmail.com>
2024-02-11 14:02:07 +0530

    fixed the roll over to another day problem

--- app/src/main/java/com/watchface/android/wearable/alpha/MainActivity.kt
+++ app/src/main/java/com/watchface/android/wearable/alpha/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.watchface.android.wearable.alpha
 
-import android.app.Activity
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
 import com.google.android.gms.fitness.data.DataType
-import com.google.android.gms.fitness.request.DataReadRequest
+import com.google.android.gms.fitness.data.Field
 import com.watchface.android.wearable.alpha.AnalogWatchFaceService.Companion.TAG
 import com.watchface.android.wearable.alpha.adapter.RecyclerAdapter
 import com.watchface.android.wearable.alpha.databinding.ActivityMainBinding
 import com.watchface.android.wearable.alpha.model.MainSchedule
 import com.watchface.android.wearable.alpha.utils.JsonParser
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), RecyclerAdapter.OnItemClickListener {
+
+    enum class FitActionRequestCode {
+        SUBSCRIBE,
+        READ_DATA
+    }
 
     private val binding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
@@ -38,11 +41,110 @@ class MainActivity : AppCompatActivity(), RecyclerAdapter.OnItemClickListener {
     private lateinit var mainSchedule: MainSchedule
     private val daysList = ArrayList<String>()
 
+    private val fitnessOptions = FitnessOptions.builder()
+        .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+        .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+        .build()
+
+    private val runningQOrLater = true
+
+    private fun oAuthPermissionsApproved() = GoogleSignIn.hasPermissions(getGoogleAccount(), fitnessOptions)
+
+    private fun getGoogleAccount() = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         initRecyclerView()
+
+        checkPermissionsAndRun(FitActionRequestCode.SUBSCRIBE)
+    }
+
+    private fun checkPermissionsAndRun(fitActionRequestCode: FitActionRequestCode) {
+        if (permissionApproved()) {
+            fitSignIn(fitActionRequestCode)
+        } else {
+            requestRuntimePermissions(fitActionRequestCode)
+        }
+    }
+
+    private fun fitSignIn(requestCode: FitActionRequestCode) {
+        if (oAuthPermissionsApproved()) {
+            performActionForRequestCode(requestCode)
+        } else {
+            requestCode.let {
+                GoogleSignIn.requestPermissions(
+                    this,
+                    requestCode.ordinal,
+                    getGoogleAccount(), fitnessOptions)
+            }
+        }
+    }
+
+    private fun readData() {
+        Fitness.getHistoryClient(this, getGoogleAccount())
+            .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+            .addOnSuccessListener { dataSet ->
+                val total = when {
+                    dataSet.isEmpty -> 0
+                    else -> dataSet.dataPoints.first().getValue(Field.FIELD_STEPS).asInt()
+                }
+                Log.i(TAG, "Total steps: $total")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "There was a problem getting the step count.", e)
+            }
+    }
+
+    private fun subscribe() {
+        // To create a subscription, invoke the Recording API. As soon as the subscription is
+        // active, fitness data will start recording.
+        Fitness.getRecordingClient(this, getGoogleAccount())
+            .subscribe(DataType.TYPE_STEP_COUNT_DELTA)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i(TAG, "Successfully subscribed!")
+                } else {
+                    Log.w(TAG, "There was a problem subscribing.", task.exception)
+                }
+            }
+    }
+
+    private fun performActionForRequestCode(requestCode: FitActionRequestCode) = when (requestCode) {
+        FitActionRequestCode.READ_DATA -> readData()
+        FitActionRequestCode.SUBSCRIBE -> subscribe()
+    }
+
+    private fun requestRuntimePermissions(requestCode: FitActionRequestCode) {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        requestCode.let {
+            if (shouldProvideRationale) {
+                Log.i(TAG, "Displaying permission rationale to provide additional context.")
+            } else {
+                Log.i(TAG, "Requesting permission")
+                // Request permission. It's possible this can be auto answered if device policy
+                // sets the permission in a given state or the user denied the permission
+                // previously and checked "Never ask again".
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                    requestCode.ordinal)
+            }
+        }
+    }
+
+    private fun permissionApproved(): Boolean {
+        val approved = if (runningQOrLater) {
+            PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACTIVITY_RECOGNITION)
+        } else {
+            true
+        }
+        return approved
     }
 
     private fun initRecyclerView() {
